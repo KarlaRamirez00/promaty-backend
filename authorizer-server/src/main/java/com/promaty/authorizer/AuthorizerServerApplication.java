e9bf5bc73153b404bd769d7ce5e601a41b27ce11
Karla Ramirez <karl.ramirez@duocuc.cl>
2026-07-18 20:34:21 -0400

    scaffolding inicial: eureka-server, user-server, authorizer-server + configuracion via variables de entorno

---
+++ authorizer-server/src/main/java/com/promaty/authorizer/AuthorizerServerApplication.java
@@ -0,0 +1,13 @@
+package com.promaty.authorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizerServerApplication.class, args);
+	}
+
+}
