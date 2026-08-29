455743b68050ae74c2ae1c98c2e69bcb1cca522d
Karla Ramirez <karl.ramirez@duocuc.cl>
2026-08-28 23:27:44 -0400

    feat: HU-20 Se agrega el esqueleto de rrhh-server
    Nuevo microservicio del dominio de negocio (RRHH), registrado en
    Eureka, con base de datos propia (promaty_rrhh).

---
+++ rrhh-server/src/main/java/com/promaty/rrhh/RrhhServerApplication.java
@@ -0,0 +1,13 @@
+package com.promaty.rrhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrhhServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrhhServerApplication.class, args);
+	}
+
+}
---
+++ rrhh-server/src/test/java/com/promaty/rrhh/RrhhServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promaty.rrhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrhhServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
